faf7a49c8f5636adebed002838f55c6befe43c58
Charles-Muia <Charles.K.Muia@gmail.com>
2023-02-13 15:10:54 +0300

    Responsive_Build_Commit

---
+++ index.php
@@ -0,0 +1,265 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@300;400&display=swap" rel="stylesheet">
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <title> Navbar | Test</title>
+</head>
+<body>
+<!--<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Top bar part><>>>>>>>>>>>>>>>>>>>>>>>>>><>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>-->
+            <div class="top-bar">
+                    <span>
+<!--                        <ion-icon name="football-outline"></ion-icon>--> <img src="Gallery/Ball.png" alt="">
+                        <p>2022/23 season</p>
+                    </span>
+<!--                                        <span><img src="" alt=""></span>-->
+                        <ul>
+                            <!--                    <li><a class="act" href="#"></a></li>-->
+                            <li><img src="Gallery/MUN_1.png" alt=""></li>
+                            <li><img src="Gallery/BFU2.png" alt=""></li>
+                            <li><img src="Gallery/Wolves2.png" alt=""></li>
+                            <!--                    <li><a href="#"></a></li>-->
+                            <li><img src="Gallery/Chelsea_FC.png" alt=""></li>
+                            <li><img src="Gallery/Brighton_&_Hove_Albion_.png" alt=""></li>
+                            <li><img src="Gallery/Leicester.webp" alt=""></li>
+
+
+                            <!--                        <li><a href="#"></a></li>-->
+                            <!--                        <li><a href="#"></a></li>-->
+                        </ul>
+            </div>
+
+<!--<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Top bar part><>>>>>>>>>>>>>>>><>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>-->
+
+            <div class="container">
+                            <div class="navbar">
+                                <img src="Gallery/P_L.png" class="logo" alt=""><span></span>
+                                        <nav>
+                                            <ul id="menuList">
+                                                <li><a href="#">home</a></li>
+                                                <li><a href="#">about us</a></li>
+                                                <li><a href="#">our work</a></li>
+                                                <li><a href="#">faqs</a></li>
+                                                <li><a href="#">contact us</a></li>
+                                            </ul>
+                                        </nav>
+                                <img src="Gallery/3781082.png" class="menu-icon" onclick="togglemenu()" alt="">
+                            </div>
+
+                <hr>
+
+<!--<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<row part><>>>>>>>>>>>>>>>>>>>>>>><>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>-->
+
+                                    <div class="row">
+                                            <div class="col-1">
+                                                <h2> EPL Match <br> Fixtures & Draws </h2>
+                                                <h3> 2022/23 Season #DeadlineDay </h3>
+                                                <p> Football's greatest season </p>
+                                                <h4> No Room For Racism </h4>
+                                                <button type="button"> Team News <img src="Gallery/logIn.png" alt=""></button>
+                                            </div>
+
+                                            <div class="col-2">
+                                                <img src="Gallery/MUNLEE.jpg" class="area-2" alt="">
+                                                    <div class="color-box"></div>
+                                                        <div class="add-btn">
+                                                            <img src="Gallery/Stadium.png" alt="">
+                                                                <p> <small> Old Trafford Stadium </small> </p>
+                                                        </div>
+                                            </div>
+                                    </div>
+
+<!--<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<about us part><<>>>>>>>>><>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>-->
+
+            <section class="wrapper">
+                    <div class="container">
+                            <div class="container-section">
+                                    <div class="title">
+                                        <h1>about us</h1>
+                                    </div>
+                                        <div class="content">
+                                            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at deleniti error,
+                                                    expedita itaque molestias non quia sit vero.ytIpsam quam, temporibus! Eius est laudantium nostrum numquam reiciendis. Facilis, vitae.
+                                                    <br><br>
+                                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores at deleniti error,
+                                                    expedita itaque molestias non quia sit vero.
+                                                    Ipsam quam, temporibus! Eius est laudantium nostrum numquam reiciendis. Facilis, vitae.
+                                                </p>
+                                                        <div class="button"><a href="#">read more <img src="" alt=""> >>>>></a></div>
+                                        </div>
+                                                        <div class="social">
+                                                            <a href=""><img src="Gallery/fb_1.png" class="fb" alt=""></a>
+                                                            <img src="Gallery/tw_1.png" class="tw" alt="">
+                                                            <img src="Gallery/yt_1.png" class="yt" alt="">
+                                                            <img src="Gallery/tw_1.png" alt="">
+                                                            <a href=""><img src="Gallery/li_1.png" class="li" alt=""></a>
+                                                        </div>
+                            </div>
+                                                <div class="image-section">
+                                                    <img src="Gallery/P_L.png" alt="">
+                                                </div>
+                    </div>
+            </section>
+
+
+                <hr>
+
+<!--<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Image Gallery part><<>>>>>>>>><>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>-->
+
+<!--                <section class="gallery-section">-->
+<!--                        <div class="img-card">-->
+<!--                            <img src="Gallery/Antony.jpg" alt="">-->
+<!--                            <label>-->
+<!--                                Antony-->
+<!--                                    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore porro repellat.</span>-->
+<!--                            </label>-->
+<!--                        </div>-->
+<!---->
+<!--                    <div class="img-card">-->
+<!--                        <img src="Gallery/27.jpg" alt="">-->
+<!--                        <label>-->
+<!--                            Antony-->
+<!--                            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore porro repellat.</span>-->
+<!--                        </label>-->
+<!--                    </div>-->
+<!--                    <div class="img-card">-->
+<!--                        <img src="Gallery/Bruno.jpg.jpg" alt="">-->
+<!--                        <label>-->
+<!--                            Antony-->
+<!--                            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore porro repellat.</span>-->
+<!--                        </label>-->
+<!--                    </div>-->
+<!--                    <div class="img-card">-->
+<!--                        <img src="Gallery/BringOn.jpg" alt="">-->
+<!--                        <label>-->
+<!--                            Antony-->
+<!--                            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi inventore porro repellat.</span>-->
+<!--                        </label>-->
+<!--                    </div>-->
+<!--                </section>-->
+
+
+
+                <div class="cont">
+                    <div class="gall">
+                        <div class="img-box" id="active"><h3>First Team</h3></div>
+                        <div class="img-box"><h3>First Team</h3></div>
+                        <div class="img-box"><h3>First Team</h3></div>
+                        <div class="img-box"><h3>First Team</h3></div>
+                        <div class="img-box"><h3>First Team</h3></div>
+                        <div class="img-box"><h3>First Team</h3></div>
+                        <div class="img-box"><h3>First Team</h3></div>
+
+                    </div>
+                </div>
+
+
+<!--<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<blog part><<>>>>>>>>><>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>-->
+
+                <section class="timer">
+                    <div class="timer-blog">
+                        <h2> latest </h2>
+                        <div class="blog-post">
+                            <div class="blog-post-img">
+                                <img src="Gallery/Harry.jpg" alt="">
+                            </div>
+                            <div class="blog-post-info">
+                                <div class="blog-post-date">
+                                    <span class="Author"> Lorem Lorem </span>
+                                    <span> Tuesday, February 7 2023</span>
+                                </div>
+                                <h1 class="blog-post-title"> Lorem ipsum dolor sit amet.</h1>
+                                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur eius minus molestias nisi qui quod reprehenderit tempora voluptas.
+                                    At culpa eligendi laboriosam libero, molestias mollitia perspiciatis ratione reiciendis soluta sunt.</p>
+                                <a href="#"class="blog-post-cta"> Read More </a>
+                            </div>
+                        </div>
+
+                        <div class="blog-post">
+                            <div class="blog-post-img">
+                                <img src="Gallery/MUNLEE.jpg" alt="">
+                            </div>
+                            <div class="blog-post-info">
+                                <div class="blog-post-date">
+                                    <span class="Author"> Match Day Live </span>
+                                    <span> Wednesday, February 8 2023</span>
+                                </div>
+                                <h1 class="blog-post-title"> Manchester United <span>vs</span> Leeds United.</h1>
+                                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur eius minus molestias nisi qui quod reprehenderit tempora voluptas.
+                                    At culpa eligendi laboriosam libero, molestias mollitia perspiciatis ratione reiciendis soluta sunt.</p>
+                                <a href="#"class="blog-post-cta"> Read More </a>
+                            </div>
+                        </div>
+
+
+                        <div class="blog-post">
+                            <div class="blog-post-img">
+                                <img src="Gallery/Ronaldo.jpg" alt="">
+                            </div>
+                            <div class="blog-post-info">
+                                <div class="blog-post-date">
+                                    <span class="Author"> 500th league goal </span>
+                                    <span> February 8 2023</span>
+                                </div>
+                                <h1 class="blog-post-title"> Ronaldo's Career High Scores.</h1>
+                                <p> Cristiano Ronaldo scores four times to move past the 500 career league goal mark as Al Nassr beat Al Wehda 4-0 in the Saudi Pro League.
+                                    <!--                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur eius minus molestias nisi qui quod reprehenderit tempora voluptas.-->
+                                    <!--                                At culpa eligendi laboriosam libero, molestias mollitia perspiciatis ratione reiciendis soluta sunt.-->
+                                </p>
+                                <a href="#"class="blog-post-cta"> Read More </a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+
+                <br><hr><br>
+
+<!--<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<footer part><<>>>>>>>>><>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>-->
+
+                <div class="foot">
+                        <section class="footer-section">
+                                <div class="footer">
+                                    <a href="#">Home</a>
+                                    <a href="#">About us</a>
+
+                                </div>
+                        </section>
+                </div>
+<!--<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Script for toggle menu part><>>>>>>>>><>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>-->
+                <script>
+
+                    var menuList = document.getElementById("menuList");
+
+                    menuList.style.maxHeight = "0px";
+
+                    function togglemenu(){
+
+                            if(menuList.style.maxHeight == "0px")
+                            {
+                                menuList.style.maxHeight = "150px";
+                            }
+                       else {
+                                menuList.style.maxHeight = "0px";
+                            }
+
+                    }
+
+                </script>
+
+</body>
+</html>
